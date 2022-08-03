ac23ff242b1b41a36c13e5f88386a685f015e771
Arjun.py <104268427+arjxn-py@users.noreply.github.com>
2022-08-03 20:27:25 +0530

    Added backend file for Action Page

---
+++ action_page.php
@@ -0,0 +1,41 @@
+<!-- The wireframe is used from cod with harry -->
+
+<?php
+$insert = false;
+if(isset($_POST['name'])){
+    // Set connection variables
+    $server = "localhost";
+    $username = "root";
+    $password = "";
+
+    // Create a database connection
+    $con = mysqli_connect($server, $username, $password);
+
+    // Check for connection success
+    if(!$con){
+        die("connection to this database failed due to" . mysqli_connect_error());
+    }
+    // echo "Success connecting to the db";
+
+    // Collect post variables
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $message = $_POST['message'];
+    $sql = "INSERT INTO `bank`.`contact` (`name`, `email`, `message`) VALUES ('$name', '$email', '$message')";
+    // echo $sql;
+
+    // Execute the query
+    if($con->query($sql) == true){
+        // echo "Successfully inserted";
+
+        // Flag for successful insertion
+        $insert = true;
+    }
+    else{
+        echo "ERROR: $sql <br> $con->error";
+    }
+
+    // Close the database connection
+    $con->close();
+}
+?>
